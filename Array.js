015b025e63d90b8333b612cdc8a2062dabf5aed5
julio.creat <cursosconcursosedu@gmail.com>
2022-12-29 19:53:38 -0300

    Quarto commit, exemplo de como importar um arquivo dentro de outro arquivo

---
+++ Array.js
@@ -0,0 +1,3 @@
+const array = [10, 2, 40, 40, 6, 7, 10];
+
+module.exports = array;
--- index.js
+++ index.js
@@ -8,10 +8,14 @@
 
 //console.log(lodash.uniq(array));
 
-const {uniq} = require('lodash');
+const {uniq} = require('lodash'); // importou a bib. lodash e desestruturou o método uniq
 
-const array = [1,2,4,4,6,7,1];
+const array = require('./array') // imprtou o arquivo array.js para dentro do arquivo index.js
+                                 // Nele, já foi exportada a constante array
+console.log(array);
 
 const arrayUnico = uniq(array);
 
-console.log(arrayUnico);
+console.log(arrayUnico); // Exibe o conteúdo da constante array, que é a mesma que foi exportada do arquivo array.js
+
+
